a984fe1d432930410128217d454578975cbddb46
Gregory Vincent Jr <52324737+GregV-crypto@users.noreply.github.com>
2023-03-08 09:41:04 -0500

    Update BisectionSearch.m

--- BisectionSearch.m
+++ BisectionSearch.m
@@ -1,41 +1,26 @@
 %Gregory Vincent
 function [root] = BisectionSearch(F, a, b, delta)
-fnA = F(a);
-fnB = F(b);
-if fnA == 0
- 
-root = Fna;
-elseif fnB == 0
- 
-root = b;
-elseif sign(a) == sign(b)
- 
-root= NaN;
+ fnA = F(a);
+ fnB = F(b);
+ if fnA == 0
+  root = Fna;
+ elseif fnB == 0
+  root = b;
+ elseif sign(a) == sign(b)
+  root= NaN;
 end
 while abs(b - a) > delta
- 
-z = (a + b) / 2;
- 
-fnZ = F(z); 
- 
-if fnZ == 0
- 
-root= z;
- 
+ z = (a + b) / 2;
+ fnZ = F(z); 
+ if fnZ == 0
+ root= z;
 end
- 
 if sign(fnZ) == sign(fnA)
- 
-a = z;
- 
-fnA = fnZ;
- 
+ a = z;
+ fnA = fnZ;
 else
- 
-b = z;
- 
-fnB = fnZ;
- 
+ b = z;
+ fnB = fnZ;
 end
 end
 root = (a + b) /2;
